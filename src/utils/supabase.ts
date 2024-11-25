19a50e19618ab3b9d4f626396a35a699d7c1e8d2
Masaki Sato <masaki@comxia.jp>
2024-11-25 15:03:03 +0900

    ✨ Supabaseクライアントの設定を更新 - 環境変数からハードコーディングされたURLとキーに変更

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  "https://uuipjavpljzwswbywqbb.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1aXBqYXZwbGp6d3N3Ynl3cWJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI0Mzg1MTEsImV4cCI6MjA0ODAxNDUxMX0.yztBEafAlCvI_BxJC3F15m3HxOxCbbgFRqUtEdpZD7g"
+);
